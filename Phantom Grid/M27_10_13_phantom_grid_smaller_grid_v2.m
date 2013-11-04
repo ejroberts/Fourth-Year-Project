395c4d5989f16551674fe0d0b00c9505850a66ce
Erica Roberts <ejroberts47@gmail.com>
2013-11-04 17:24:50 +0000

    Included the effects of kinetic energy of the particle on the force

--- Phantom Grid/M27_10_13_phantom_grid_smaller_grid_v2.m
+++ Phantom Grid/M27_10_13_phantom_grid_smaller_grid_v2.m
@@ -4,16 +4,16 @@ for a=1:1:n
     for b=1:1:n
            l=l+1;
            A(a,b)=rand;                                     %brownian seed function
-           B(a,b)=rand;                                      %potential seed function
+           B0(a,b)=rand;                                      %potential seed function
+           B(a,b)=B0(a,b);
            position(l,1)=a;                                 %Storing the x position of the grid 
            position(l,2)=b;                                 %Storing the y position of the grid
     end
 end
-
 q=1;                                                        %Particle charge
 m=1;                                                        %Particle mass
-particlex=5.5 ;                                             %initial particle x-position
-particley=1 ;                                               %initial particle y-position
+particlex=3;                                            %initial particle x-position
+particley=3;                                               %initial particle y-position
 c=0.1;                                                       %Values defined for parameters in stochastic random walk
 d=0.9;
 dt=pi/4;                                                     %Time step
@@ -25,21 +25,22 @@ for t=0:dt:2*pi
          l=l+1;                                             %Move to next row in the array
          fprintf('%f',t);
          brownian=A(j,k)+(sqrt(dt))*(randn);   %Calculates brownian motion 
-         potential=B(j,k)*exp(c-0.5*(d^2))*t+d*brownian;    %Calculates potential using stochastic
+         potential=B0(j,k)*exp(c-0.5*(d^2))*t+d*brownian;    %Calculates potential using stochastic
          position(l,3)=potential;
          A(j,k)=brownian;
+         B(j,k)=potential;
        end 
    end
-end
+
 for l=1:1:n
-    A(l,n)=A(l,2)
-    A(l,1)=A(l,n-1)
-    A(1,l)=A(n-1,l)
-    A(n,l)=A(2,l)
-    A(n,n)=A(2,2)
-    A(1,1)=A(n-1,n-1)
-    A(n,1)=A(2,n-1)
-    A(1,n)=A(n-1,2)
+    B(l,n)=B(l,2)
+    B(l,1)=B(l,n-1)
+    B(1,l)=B(n-1,l)
+    B(n,l)=B(2,l)
+    B(n,n)=B(2,2)
+    B(1,1)=B(n-1,n-1)
+    B(n,1)=B(2,n-1)
+    B(1,n)=B(n-1,2)
 end
 
 
@@ -54,11 +55,11 @@ if (particlex<=n-1 && particlex>=2)                                           %F
         particley=particley
         
     elseif(particley>n-1)
-        particley=particley-(n-1)
+        particley=particley-(n-2)
         particlex=particlex
      
     else
-        particley=particley+(n-1)
+        particley=particley+(n-2)
         particlex=particlex
     end
 
@@ -69,36 +70,52 @@ elseif (particley<=n-1 && particley>=2)
         particley=particley
         
     elseif(particlex>n-1)
-        particlex=particlex-(n-1)
+        particlex=particlex-(n-2)
         particley=particley
      
     else(particlex<2)
-        particlex=particlex+(n-1)
+        particlex=particlex+(n-2)
         particley=particley
      end
 elseif(particlex>(n-1))
     if (particley>(n-1))
-        particlex=particlex-(n-1)
-        particley=particley-(n-1)
+        particlex=particlex-(n-2)
+        particley=particley-(n-2)
     
     elseif (particley<2)
-        particlex=particlex-(n-1)
-        particley=particley+(n-1)
+        particlex=particlex-(n-2)
+        particley=particley+(n-2)
         
     end
 elseif (particlex<2)
     if (particley>(n-1))
-        particlex=particlex+(n-1)
-        particley=particley-(n-1)
+        particlex=particlex+(n-2)
+        particley=particley-(n-2)
     
     elseif (particley<2)
-        particlex=particlex+(n-1)
-        particley=particley+(n-1)
+        particlex=particlex+(n-2)
+        particley=particley+(n-2)
         
     end
 end
+    [gx, gy] = differentiate(sf, [particlex,particley])                     %Electric field calculated as derivative of potential at particle position
+     gx=-ex
+     gy=-ey
+     ekx=0.5*m*vx^2
+     eky=0.5*m*vy^2
+     dx=0.5*ax*(dt)^2
+     dy=0.5*ay*(dt)^2
+     fx = q*ex+ekx/dx                                                              %Force at particle position in x direction
+     fy = q*ey+eky/dy                                                              %Force at particle position in y direction
+     ax = fx/m                                                              %Acceleration of particle in x direction
+     ay = fy/m                                                              %Acceleration of particle in y direction 
+     vx=vx+ax*dt                                                            %Velocity in x direction
+     vy=vy+ay*dt                                                            %Velocity in y direction
+     particlex = particlex+0.5*ax*(dt)^2                                      %Particle position along the x axis
+     particley = particley+0.5*ay*(dt)^2                                      %Particle position along the y axis
+    
 
-
+     end
 
 
 
