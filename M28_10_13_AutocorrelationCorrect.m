3255e358f06cb909aef67ea7e70fdf5f2a48996c
Erica Roberts <ejroberts47@gmail.com>
2013-11-04 12:02:05 +0000

    Code now works to correctly compute the autocorrelation for a changing lag number

--- M28_10_13_AutocorrelationCorrect.m
+++ M28_10_13_AutocorrelationCorrect.m
@@ -3,7 +3,6 @@ dt=pi/50;
 potential(1)=rand;           %Initial random potential
 S(1)=rand;
 n=10;                         %Number of steps
-i=1;
 a=0.1;
 b=0.9;
 
@@ -13,11 +12,17 @@ b=0.9;
         S(k)=S(1)*exp((a-0.5*(b^2))*t(k)+b*potential(k)); 
     end
     
- M=0;
- N=0;
+
     
+ 
+ for i=1:1:5
+ M=0;
+ N=0; 
+ a=zeros([1,n])
+ b=zeros([1,n])
+ 
      for k=1:1:n-i
-        M=M+S(k)
+         M=M+S(k)
     end
  
     Meana=M/(n-i)
@@ -25,7 +30,7 @@ b=0.9;
     for k=1:1:n-i
         a(k)=(S(k)-Meana)
     end
-   
+    
     
     for k=1+i:1:n
         N=N+S(k)
@@ -44,7 +49,8 @@ b=0.9;
 
 num=sum(a.*b)
  
- autocorrelation=num/den
+ autocorrelation(i)=num/den
+ end
  
 
 
